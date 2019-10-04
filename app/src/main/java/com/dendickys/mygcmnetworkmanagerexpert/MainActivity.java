24c1a1fd8186b5e917b9c3d503a9297c61d3b2bc
dendickys <febri.unquad2014@gmail.com>
2019-10-05 02:55:03 +0700

    My GCM Network Manager finish, but now is Deprecated :(

--- app/src/main/java/com/dendickys/mygcmnetworkmanagerexpert/MainActivity.java
+++ app/src/main/java/com/dendickys/mygcmnetworkmanagerexpert/MainActivity.java
@@ -3,12 +3,38 @@ package com.dendickys.mygcmnetworkmanagerexpert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSetSchedule, btnCancelSchedule;
+    private SchedulerTask mSchedulerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSetSchedule = findViewById(R.id.btn_set_scheduler);
+        btnCancelSchedule = findViewById(R.id.btn_cancel_scheduler);
+        btnSetSchedule.setOnClickListener(this);
+        btnCancelSchedule.setOnClickListener(this);
+
+        mSchedulerTask = new SchedulerTask(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_set_scheduler) {
+            mSchedulerTask.createPeriodicTask();
+            Toast.makeText(this, "Periodic Task Created", Toast.LENGTH_SHORT).show();
+        }
+
+        if (v.getId() == R.id.btn_cancel_scheduler) {
+            mSchedulerTask.cancelPeriodicTask();
+            Toast.makeText(this, "Periodic Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
